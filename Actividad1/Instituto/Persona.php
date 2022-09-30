0e63f40cb7a15811d2c98a13265aa994b913c7b0
root4554 <anass.kbir@hotmail.com>
2022-09-30 14:26:42 +0200

    add changes

--- Actividad1/Instituto/Persona.php
+++ Actividad1/Instituto/Persona.php
@@ -26,11 +26,18 @@ class Persona
         $this->dni = $dni;
         $this->email = $email;
     }
+    // public function printHtml(){
+
+    //     echo "<p>Nombre: $this->nombre</p>";
+    //     echo "<p>DNI: $this->dni</p>";
+    //     echo "<p>Email: $this->email</p>";
+    // }
     public function printHtml(){
-        echo "<p>Nombre: $this->nombre</p>";
-        echo "<p>DNI: $this->dni</p>";
-        echo "<p>Email: $this->email</p>";
+        echo "<table border='1'>";
+        echo "<tr><td>Nombre</td><td>DNI</td><td>Email</td></tr>";
+        echo "<tr><td>$this->nombre</td><td>$this->dni</td><td>$this->email</td></tr>";
     }
+
 
     public function toHtml(){
         return "<p>Nombre: $this->nombre</p>
@@ -47,6 +54,34 @@ class Persona
         $c = file_get_contents($fichero);
         $personas = unserialize($c);
         return $personas;
+    }
+    //Borrar una persona de un array de personas
+    public static function borrarPersona($personas, $dni){
+        $personas = array_filter($personas, function($persona) use ($dni){
+            return $persona->dni != $dni;
+        });
+        guardarPersonas("personas.txt", $personas);
+        return $personas;
+    }
+
+    //modificar una persona de un array de personas
+    public static function modificarPersona($personas, $dni, $nombre, $email){
+        $personas = array_map(function($persona) use ($dni, $nombre, $email){
+            if($persona->dni == $dni){
+                $persona->nombre = $nombre;
+                $persona->email = $email;
+            }
+            return $persona;
+        }, $personas);
+        guardarPersonas("personas.txt", $personas);
+        return $personas;
+    }
+
+    //mostrar personas
+    public static function mostrarPersonas($personas){
+        foreach($personas as $persona){
+            $persona->printHtml();
+        }
     }
 }
 
--- Actividad1/Instituto/insertPers.php
+++ Actividad1/Instituto/insertPers.php
@@ -1,42 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-        <form >
-            <input name="nombre" type="text" placeholder="Nombre">
-            <input name="DNI" type="text" placeholder="DNI">
-            <input name="email" type="text" placeholder="Email">
-            <input type="submit" value="Guardar">
-            <input type="submit" value="Borrar">
-            <input type="submit" value="Cancelar">
-        </form>
-        <?php
+    <form>
+        <input name="nombre" type="text" placeholder="Nombre">
+        <input name="DNI" type="text" placeholder="DNI">
+        <input name="email" type="text" placeholder="Email">
+        <input type="submit" value="Guardar" name="accion">
+        <input type="submit" value="Borrar" name="accion">
+        <input type="submit" value="Editar">
+        <input type="submit" value="Mostrar">
+        <input type="submit" value="Ocultar">
+    </form>
+
+    <?php
             use Instituto\Persona;
 
-include 'Persona.php';
-$personas=Persona::leerPersonas("ListaPersonas.txt");
-if($personas!=null){
-        //imprimir datos
-        foreach($personas as $persona){
-            echo $persona->printHTml();
-        }
-}else{
-    echo "<p style='color:red;'>No hay personas</p>";}
-        
+        include 'Persona.php';
 
-            if( isset($_GET['nombre']) && isset($_GET['DNI']) && isset($_GET['email']) ){
-                $nuevapersona = new Persona($_GET['nombre'], $_GET['DNI'], $_GET['email']);
-                $nuevapersona->printHTml();
-                $personas[] = $nuevapersona;
-                //array_push($personas, $nuevapersona);
-                Persona::guardarPersonas('ListaPersonas.txt',$personas);
+        $personas=Persona::leerPersonas("ListaPersonas.txt");
 
+        //buscar si existe el dni
+        function buscarPersona($personas, $dni){
+    foreach($personas as $persona){
+                if($persona->dni == $dni){
+                    return $persona;
+                }
             }
-            ?>
+            return null;
+        }
+
+        //guardar una persona en un array de personas
+        function guardarPersona($personas, $persona){
+            if( isset($_GET['nombre']) && isset($_GET['DNI']) && isset($_GET['emai
+                if(!buscarPersona($personas, $_GET['DNI'])){
+                    $nuevapersona = new Persona($_GET['nombre'], $_GET['DNI'], $_GET['email']);
+                    $nuevapersona->printHTml();
+                    $personas[] = $nuevapersona;
+                    echo '<script>alert("Has guardado la persona correctamente")</script>';
+                    Persona::guardarPersonas('ListaPersonas.txt',$personas);
+                }
+                else{
+                    echo '<script>alert("Ya Existe una persona con este DNI")</script>';
+                }
+            }
+        }
+
+        //imprimir datos
+        if($personas!=null){
+                
+                foreach($personas as $persona){
+                    echo $persona->printHTml();
+                }
+        }else{
+            echo "<p style='color:red;'>No hay personas</p>";}
+                    ?>
 </body>
+
 </html>
